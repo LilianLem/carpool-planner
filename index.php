39b5145fb6f54ef52ebfb0f4a49ac22ecada6529
LilianLem <54894780+LilianLem@users.noreply.github.com>
2020-04-19 23:33:23 +0200

    MVC | Ajout index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+try {
+	// if(isset($_GET['action']))
+	// {
+		require('router/router.php');
+	// }
+	// else
+	// {
+	//	require('router/router.php');
+	// }
+}
+
+catch(Exception $e) {
+    // Si une erreur se produit, on arrive ici
+    echo "Une erreur est survenue.<br>Détails : $e";
+}
