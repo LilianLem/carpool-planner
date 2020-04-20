c1e8903200e8411b551cfe505e08f541e7529d62
LilianLem <54894780+LilianLem@users.noreply.github.com>
2020-04-20 10:59:01 +0200

    MVC | Model ApiManager : correction appel API
    Problème d'appel avec les lettres accentuées contenues dans certains noms de villes

--- model/ApiManager.php
+++ model/ApiManager.php
@@ -6,7 +6,7 @@ class ApiManager
 {
 	public function checkCity($city,$department)
 	{
-		$result = file_get_contents('https://geo.api.gouv.fr/communes?nom='.$city.'&codeDepartement='.$department.'&fields=nom,centre,codeDepartement&format=json&geometry=centre');
+		$result = file_get_contents('https://geo.api.gouv.fr/communes?nom='.urlencode($city).'&codeDepartement='.$department.'&fields=nom,centre,codeDepartement&format=json&geometry=centre');
 
 		if(!$result or empty($result))
 		{
