24495adf50943206d89ee382e0f95daa181b1ad2
LilianLem <54894780+LilianLem@users.noreply.github.com>
2020-04-20 09:44:36 +0200

    MVC | Mise à jour controller
    Ajout de checkProposalAdd()

--- controller/controller.php
+++ controller/controller.php
@@ -13,3 +13,15 @@ function displayProposalAddForm()
 {
 	require('view/proposalAdd.php');
 }
+
+function checkProposalAdd()
+{
+	if(isset($_POST['discord-username']) AND isset($_POST['city']) AND isset($_POST['department']) AND isset($_POST['date']) AND isset($_POST['time']))
+	{
+	}
+
+	else
+	{
+	}
+}
+}
