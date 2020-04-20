9f6ce5ff01d1883cca89de4a103c7ce2dbea34a0
LilianLem <54894780+LilianLem@users.noreply.github.com>
2020-04-20 09:59:42 +0200

    MVC | Ajout model ApiManager
    Connexion avec l'API Géo du gouvernement pour vérifier la ville et récupérer des informations

---
+++ model/ApiManager.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'vendor/autoload.php';
+
+class ApiManager
+{
+	public function checkCity($city,$department)
+	{
+		$result = file_get_contents('https://geo.api.gouv.fr/communes?nom='.$city.'&codeDepartement='.$department.'&fields=nom,centre,codeDepartement&format=json&geometry=centre');
+
+		if(!$result or empty($result))
+		{
+			return false;
+		}
+
+		return $result;
+	}
+}
