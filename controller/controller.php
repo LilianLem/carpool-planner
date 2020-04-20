c20a5670626f96b16c1499b42a2af883fdda6604
LilianLem <54894780+LilianLem@users.noreply.github.com>
2020-04-20 09:45:29 +0200

    MVC | Controller : validation du pseudo Discord

--- controller/controller.php
+++ controller/controller.php
@@ -18,6 +18,10 @@ function checkProposalAdd()
 {
 	if(isset($_POST['discord-username']) AND isset($_POST['city']) AND isset($_POST['department']) AND isset($_POST['date']) AND isset($_POST['time']))
 	{
+		if(!stristr($_POST['discord-username'], '#'))
+		{
+			$errors .= "- Le format du pseudo Discord est incorrect. Exemple correct : Pierre#1234\n";
+		}
 	}
 
 	else
